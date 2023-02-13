2de79e66471ed982e2cd2f50af8ccc6a8f26b391
gopal <gopalmule9707>
2023-02-13 15:56:37 +0530

    first commit

--- Hellow.java
+++ Hellow.java
@@ -1,6 +1,3 @@
-# demo-project
-A basic introduction get regarding git and Git Hub
-Hellow 
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
